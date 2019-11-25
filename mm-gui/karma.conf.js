cc92dc82effa3fe69175c525af135907111fceb7
Guilherme Calland Rosa Borba <gcrb@ug3c06.windows.cin.ufpe.br>
2019-11-25 13:19:14 -0300

    resolvendo login

--- mm-gui/karma.conf.js
+++ mm-gui/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/ta-gui'),
+      dir: require('path').join(__dirname, './coverage/mm-gui'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
