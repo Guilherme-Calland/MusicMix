044f7764f67b6850545a0ff8df73837a83df2d3f
guilhermow <gcrb@cin.ufpe.br>
2019-11-29 21:17:48 -0300

    limpando codigo

--- mm-gui/src/app/musicians.component.ts
+++ mm-gui/src/app/musicians.component.ts
@@ -35,75 +35,27 @@ import { MusicianService } from './musician.service';
     this.musicianService.check(m).subscribe(
     ar => {
       if(ar) {
-        if(ar.password == m.password){
           window.location.replace('/perfil');
-        } else {
-          this.invalidlogin = true;
-        }
-        
         } else {
           this.invalidlogin = true;
         }
       }
     )
-
-
-      
-      // this.invalidlogin = this.usernameNotSubscribed(musician.username);
-
-      // if(this.invalidlogin) return;
-
-      // var mus = this.getMusicianFromMusicians(musician.username);
-
-      // if(this.validPassword(mus.password)){
-      //   window.location.replace('/perfil');
-      // } else {
-      //   this.invalidlogin = true;
-      // }
-
-    }
-
-    usernameNotSubscribed(username: string): boolean {
-      if(!this.musicians.find(a => a.username == username)){
-        return true;
-      }
-      else return false;
-    }
-
-    validPassword(password: string): boolean {
-      if(this.musician.password == password) return true;
-      else return false;  
-    }
-
-    checkpassword(password: string): void{
-
-    }
-
-
-    getMusicianFromMusicians(username: string): Musician {
-      // var i;
-      // for(i = 0; i < this.musicians.length; i++){
-      //   if (this.musicians[i].username == username){
-      //     return this.musician[i];
-      //   } 
-      // }
-      return this.musicians.find(a => a.username ==username);
-      return null;
-    }
-
-    onMove(): void {
-       this.duplicatedusername = false;
-       this.invalidlogin = false;
-    }
-
-     ngOnInit(): void {
-       this.musicianService.getMusicians()
-             .subscribe(
-               as => { this.musicians = as; },
-               msg => { alert(msg.message); }
-              );
-     }
-
-
-
   }
+
+  onMove(): void {
+      this.duplicatedusername = false;
+      this.invalidlogin = false;
+  }
+
+  ngOnInit(): void {
+    this.musicianService.getMusicians()
+          .subscribe(
+            as => { this.musicians = as; },
+            msg => { alert(msg.message); }
+          );
+  }
+
+
+
+}
