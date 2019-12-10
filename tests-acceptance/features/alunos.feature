cae616c0d649c3809c6f09f84b533fc5b6b0188f
evalldojunior <evalldojn@gmail.com>
2019-12-10 19:09:40 -0300

    implementando teste 2

--- tests-acceptance/features/alunos.feature
+++ tests-acceptance/features/alunos.feature
@@ -2,8 +2,7 @@ Feature: As a professor
          I want to register students
          So that I can manage their learning goals
 
-Scenario: Registering student with non registered CPF
-Given I am at the students page
-Given I cannot see a student with CPF "683" in the students list
-When I try to register the student "Paulo" with CPF "683"
-Then I can see "Paulo" with CPF "683" in the students list
+Scenario: Registering musician
+Given I am at the login page
+When I try to register the musician with username "guilherme" and password "123"
+Then I can see the profile page
--- tests-acceptance/stepdefinitions/musicians.ts
+++ tests-acceptance/stepdefinitions/musicians.ts
@@ -14,26 +14,30 @@ defineSupportCode(function ({ Given, When, Then }) {
     Given(/^I am at the login page$/, async () => {
         await browser.get("http://localhost:4200/login");
         await expect(browser.getTitle()).to.eventually.equal('MusicMix');
-        await $("m[name='musicians']").click();
     })
 
-    Given(/^I cannot see a student with CPF "(\d*)" in the students list$/, async (cpf) => {
-        var allcpfs : ElementArrayFinder = element.all(by.name('cpflist'));
-        await allcpfs;
-        var samecpfs = allcpfs.filter(elem =>
-                                      elem.getText().then(text => text === cpf));
-        await samecpfs;
-        await samecpfs.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(0));
+    // Given(/^I cannot see a student with CPF "(\d*)" in the students list$/, async (cpf) => {
+    //     var allcpfs : ElementArrayFinder = element.all(by.name('cpflist'));
+    //     await allcpfs;
+    //     var samecpfs = allcpfs.filter(elem =>
+    //                                   elem.getText().then(text => text === cpf));
+    //     await samecpfs;
+    //     await samecpfs.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(0));
+    // });
+
+    When(/^I try to register the musician with username "([^\"]*)" and password "(\d*)"$/, async (username, password) => {
+        await $("input[name='username']").sendKeys(<string> username);
+        await $("input[name='password']").sendKeys(<string> password);
+        await element(by.buttonText('Cadastrar')).click();
     });
 
-    When(/^I try to register the student "([^\"]*)" with CPF "(\d*)"$/, async (name, cpf) => {
-        await $("input[name='namebox']").sendKeys(<string> name);
-        await $("input[name='cpfbox']").sendKeys(<string> cpf);
-        await element(by.buttonText('Adicionar')).click();
-    });
+    // Then(/^I can see "([^\"]*)" with CPF "(\d*)" in the students list$/, async (name, cpf) => {
+    //     var allalunos : ElementArrayFinder = element.all(by.name('alunolist'));
+    //     allalunos.filter(elem => pAND(samePassword(elem,cpf),sameUserName(elem,name))).then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+    // });
 
-    Then(/^I can see "([^\"]*)" with CPF "(\d*)" in the students list$/, async (name, cpf) => {
-        var allalunos : ElementArrayFinder = element.all(by.name('alunolist'));
-        allalunos.filter(elem => pAND(samePassword(elem,cpf),sameUserName(elem,name))).then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+    Then(/^I can see the profile page$/, async () => {
+        await browser.get("http://localhost:4200/profile");
+        await expect(browser.getTitle()).to.eventually.equal('MusicMix');
     });
 })
