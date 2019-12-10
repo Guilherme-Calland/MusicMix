2c99ea202f72f96fe3d4290b9563babb20ba6cbb
Victoria Celia Cesar Ferreira <vccf@ug4c07.windows.cin.ufpe.br>
2019-12-10 17:55:05 -0300

    implementando testes

---
+++ tests-acceptance/stepdefinitions/musicians.ts
@@ -0,0 +1,39 @@
+import { defineSupportCode } from 'cucumber';
+import { browser, $, element, ElementArrayFinder, by } from 'protractor';
+let chai = require('chai').use(require('chai-as-promised'));
+let expect = chai.expect;
+
+let sleep = (ms => new Promise(resolve => setTimeout(resolve, ms)));
+
+let samePassword = ((elem, password) => elem.element(by.name('passwordlist')).getText().then(text => text === password));
+let sameUserName = ((elem, username) => elem.element(by.name('namelist')).getText().then(text => text === username));
+
+let pAND = ((p,q) => p.then(a => q.then(b => a && b)))
+
+defineSupportCode(function ({ Given, When, Then }) {
+    Given(/^I am at the login page$/, async () => {
+        await browser.get("http://localhost:4200/login");
+        await expect(browser.getTitle()).to.eventually.equal('MusicMix');
+        await $("m[name='musicians']").click();
+    })
+
+    Given(/^I cannot see a student with CPF "(\d*)" in the students list$/, async (cpf) => {
+        var allcpfs : ElementArrayFinder = element.all(by.name('cpflist'));
+        await allcpfs;
+        var samecpfs = allcpfs.filter(elem =>
+                                      elem.getText().then(text => text === cpf));
+        await samecpfs;
+        await samecpfs.then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(0));
+    });
+
+    When(/^I try to register the student "([^\"]*)" with CPF "(\d*)"$/, async (name, cpf) => {
+        await $("input[name='namebox']").sendKeys(<string> name);
+        await $("input[name='cpfbox']").sendKeys(<string> cpf);
+        await element(by.buttonText('Adicionar')).click();
+    });
+
+    Then(/^I can see "([^\"]*)" with CPF "(\d*)" in the students list$/, async (name, cpf) => {
+        var allalunos : ElementArrayFinder = element.all(by.name('alunolist'));
+        allalunos.filter(elem => pAND(samePassword(elem,cpf),sameUserName(elem,name))).then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+    });
+})
